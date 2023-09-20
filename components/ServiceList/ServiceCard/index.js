777a5830f9886b52c6c6a86c1e2695d0e1530c6d
Shawn Huelle <shuelle@posteo.de>
2023-09-20 11:26:12 +0200

    add ServiceCard

---
+++ components/ServiceList/ServiceCard/index.js
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+export default function ServiceCard() {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, isLoading, error } = useSWR(`/api/services/${id}`);
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    return;
+  }
+
+  return (
+    <>
+      <h2>Service</h2>
+      <article>
+        <Image
+          src={service.image}
+          width={100}
+          height={100}
+          alt="picture of the service"
+        />
+        <h3>{data.name}</h3>
+        <p>{data.description}</p>
+        <p>{data.date}</p>
+        <p>{data.time}</p>
+        <p>{data.price} €</p>
+      </article>
+      <Link href="/">← back</Link>
+    </>
+  );
+}
