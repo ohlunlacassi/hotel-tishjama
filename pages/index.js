968d0a65ae3ae4d478987c996d0f0cf6e8840f28
Phatjira Rungsakullikit <pjr_oh@hotmail.com>
2023-09-20 10:08:21 +0200

    delete fragments

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
 import ServiceList from "../components/ServiceList/index";
 
 export default function HomePage() {
-  return (
-    <>
-      <ServiceList />
-    </>
-  );
+  return <ServiceList />;
 }
