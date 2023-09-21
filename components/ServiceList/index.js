9298ea8216a551ae03f7e2c43a198e2646ea0fc1
Tien Pham <tienphamvanfuv@gmail.com>
2023-09-21 10:20:28 +0200

    add Link from homepage to detail pages

--- components/ServiceList/index.js
+++ components/ServiceList/index.js
@@ -1,5 +1,6 @@
 import useSWR from "swr";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ServiceList() {
   const { data, isLoading, error } = useSWR("/api/services");
@@ -26,13 +27,15 @@ export default function ServiceList() {
       <ul>
         {data.map((service) => (
           <li key={service._id}>
-            <Image
-              src={service.image}
-              width={100}
-              height={100}
-              alt="picture of the service"
-            />
-            {service.name}
+            <Link href={`/${service._id}`}>
+              <Image
+                src={service.image}
+                width={100}
+                height={100}
+                alt="picture of the service"
+              />
+              {service.name}
+            </Link>
           </li>
         ))}
       </ul>
