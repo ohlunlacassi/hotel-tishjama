3390dc1b28b9daccb13b29b30b8fb1dccea30528
Shawn Huelle <shuelle@posteo.de>
2023-09-28 14:20:38 +0200

    add easy user switching and some text on the ud

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import ServiceList from "../components/ServiceList/index";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <>
+      <p>
+        Welcome! On this page, you will find a list of Hotel Tishjama's
+        bookable, luxury services! Enjoy your stay.
+      </p>
+      <Link href="/SPD">Service Provider Dashboard</Link>
       <h2>Services</h2>
       <ServiceList />
     </>
