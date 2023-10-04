3ea454b0ef0f3905a43d38dea0e57bb9b2d8f0b3
Shawn Huelle <shuelle@posteo.de>
2023-10-04 16:55:23 +0200

    add user structure

--- pages/MyBookings/index.js
+++ pages/MyBookings/index.js
@@ -1,7 +1,8 @@
 import MyBookingList from "@/components/MyBookings";
 import Link from "next/link";
 
-export default function MyBookings() {
+export default function MyBookings({ user }) {
+  console.log("USER: ", user);
   return (
     <>
       <MyBookingList />
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,18 @@
 import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
+import useUser from "@/hooks/useUser";
 
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { user } = useUser();
+
   return (
     <>
       <SWRConfig value={{ fetcher }}>
         <GlobalStyle />
         <h1>Hotel Tishjama</h1>
-        <Component {...pageProps} />
+        <Component {...pageProps} user={user} />
       </SWRConfig>
     </>
   );
