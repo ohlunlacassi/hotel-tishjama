2945146882cd0856d9d832355a4afd3fdaad3950
Shawn Huelle <shuelle@posteo.de>
2023-10-06 11:58:04 +0200

    change function to variable

--- pages/[id].js
+++ pages/[id].js
@@ -5,18 +5,22 @@ import Link from "next/link";
 export default function ServiceDetailsPage({ user }) {
   const router = useRouter();
   const { id } = router.query;
-  function isBooked() {
-    if (!user) {
-      return;
-    }
-    return user.bookings.find((booking) => booking._id === id);
-  }
+
+  const isBooked = user
+    ? user.bookings.find((booking) => booking._id === id)
+    : null;
+  // function isBooked() {
+  //   if (!user) {
+  //     return;
+  //   }
+  //   return user.bookings.find((booking) => booking._id === id);
+  // }
   return (
     <>
       <p>Here are the details of the luxury service you selected.</p>
       <ServiceCard id={id} user={user} isBooked={isBooked} />
 
-      {isBooked() ? (
+      {isBooked ? (
         <Link href="/MyBookings">← My Bookings</Link>
       ) : (
         <Link href="/">← Services</Link>
