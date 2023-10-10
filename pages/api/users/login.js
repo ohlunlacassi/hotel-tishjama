e4216b934813eb8c0c29341e422c9157bc7d4b16
Tien Pham <tienphamvanfuv@gmail.com>
2023-10-10 09:37:26 +0200

    add findOne

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,11 +1,14 @@
+import User from "@/db/models/User";
+
 export default function handler(request, response) {
   if (request.method === "POST") {
-    const user = findByEmail(request.body.email);
+    const user = User.findOne({ email: request.body.email });
     if (!user) {
       response.status(400).json({ message: "Incorrect Credentials" });
+      console.log("check:", user);
       return;
     }
-    if (request.body.roomNumber !== roomNumber) {
+    if (request.body.roomNumber !== user.roomNumber) {
       response.status(400).json({ message: "Incorrect Credentials" });
       return;
     }
