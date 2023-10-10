a81592ea7f1826700b4c2213c747dedb06021a7f
Shawn Huelle <shuelle@posteo.de>
2023-10-10 10:10:43 +0200

    reorder line 16 in login.js

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -3,6 +3,7 @@ import User from "@/db/models/User";
 export default function handler(request, response) {
   if (request.method === "POST") {
     const user = User.findOne({ email: request.body.email });
+    console.log("USER: ", user);
     if (!user) {
       response.status(400).json({ message: "Incorrect Credentials" });
       console.log("check:", user);
@@ -12,8 +13,7 @@ export default function handler(request, response) {
       response.status(400).json({ message: "Incorrect Credentials" });
       return;
     }
-    response.status(200).json(user);
-    return;
+    return response.status(200).json(user);
   }
   response.status(405).json({ message: "Method Not Allowed" });
 }
